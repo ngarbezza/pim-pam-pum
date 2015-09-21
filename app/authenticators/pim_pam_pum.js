ab5aa6dbbd0c57964ebbe9488d46a7ba1ac6f7f6
Nahuel Garbezza <n.garbezza@gmail.com>
2015-09-20 22:26:28 -0300

    armar request correcto para login

--- app/authenticators/pim_pam_pum.js
+++ app/authenticators/pim_pam_pum.js
@@ -1,6 +1,16 @@
 import TokenAuthenticator from 'simple-auth-token/authenticators/token';
 
 export default TokenAuthenticator.extend({
+
+  getAuthenticateData: function(credentials) {
+    var authenticationRoot = {};
+    var authentication = {};
+    authentication[this.passwordField] = credentials.password;
+    authentication[this.identificationField] = credentials.identification;
+    authenticationRoot['user'] = authentication;
+    return authenticationRoot;
+  },
+
   invalidate: function(data) {
     console.log('invalidate')
   }
