980bf8c73910207f0c697f3d75f6af7b1b99933d
Nahuel Garbezza <n.garbezza@gmail.com>
2015-09-26 10:54:15 -0300

    pruebas con ember-validation

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,8 +1,26 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations, {
+  errors: [],
+
+  validations: {
+    email: {
+      presence: true,
+      format: {
+        with: /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i,
+        message: 'debe ser una dirección de e-mail válida'
+      }
+    },
+    password: {
+      presence: true,
+      length: {minimum: 8}
+    },
+    passwordConfirmation: true
+  },
+
   actions: {
-    createUser: function() {
+    createUser: function () {
       var email = this.get('userEmail');
       var password = this.get('userPassword');
 
