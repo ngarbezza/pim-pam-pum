b2af3301d06bb541568c8adf7a8c3483991b524c
rocioRedbee <rocio.amaya@redb.ee>
2015-10-13 00:32:46 -0300

    stilos de login, registrar y header nav

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,5 +19,13 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+
+
+  // for glyphicons
+  app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
+  app.import('bower_components/bootstrap-sass-official/assets/fonts/bootstrap/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts/bootstrap/'
+  });
+
   return app.toTree();
 };
