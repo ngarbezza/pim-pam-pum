ba43a087dfdace9cbe0d9070bad29aedb29a6f00
Nahuel Garbezza <n.garbezza@gmail.com>
2015-11-08 20:48:50 -0300

    arreglar problemas de posicionamiento, locale y validaciones en el datetimepicker de crear evento

--- app/controllers/nuevo-evento.js
+++ app/controllers/nuevo-evento.js
@@ -1,34 +1,51 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations, {
+
+  validations: {
+    nombre: {
+      presence: {
+        message: 'ingrese el nombre del evento'
+      }
+    },
+    fecha: {
+      presence: {
+        message: 'ingrese la fecha y hora'
+      }
+    },
+    direccion: {
+      presence: {
+        message: 'ingrese la dirección'
+      }
+    }
+  },
+
   actions: {
     crear: function () {
       var nombreEvento = this.get('nombre');
       var descripcion = this.get('descripcion');
       var fecha = this.get('fecha');
-      var direccion = this.get('direccion')
+      var direccion = this.get('direccion');
 
-      var evento = this.store.createRecord('Evento', {
+      var evento = this.store.createRecord('evento', {
         nombre: nombreEvento,
         descripcion: descripcion,
-        fecha:fecha,
+        fecha: fecha,
         direccion: direccion
-
       });
       this.set('model', evento);
 
-      evento.save();
-      if(!evento.isError){
-
-        this.set('nombre', undefined);
-        this.set('descripcion', undefined);
-        this.set('fecha', undefined);
-        this.set('direccion', undefined)
-        this.transitionToRoute('eventos');
-      }
-
+      var self = this;
+      evento.save().then(function () {
+        self.set('nombre', '');
+        self.set('descripcion', '');
+        self.set('fecha', '');
+        self.set('direccion', '');
+        self.transitionToRoute('eventos');
+      }, function () {
+        self.set('model', evento);
+      });
     }
   }
-
-
 });
