00e8ef71f8aa3800fc99f0392c537d6d7387f841
rocioRedbee <rocio.amaya@redb.ee>
2015-11-18 21:48:07 -0300

    fixeo de detalle de evento y estilos y boton de retorno

--- app/controllers/evento-detalle.js
+++ app/controllers/evento-detalle.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
+import moment from 'moment';
+
 
 var EventoDetalleController = Ember.Controller.extend({
-  hayConfirmados: Ember.computed.notEmpty('confirmados')
+  hayConfirmados: Ember.computed.notEmpty('confirmados'),
+
+  fechaFormateada:  function () {
+    return moment(this.get('fecha')).format('DD/MM/YYYY');
+  }.property('fecha'),
+
+  actions: {
+    volver: function () {
+      this.transitionToRoute('eventos');
+    }
+  }
 });
 
 export default EventoDetalleController;
--- app/routes/evento-detalle.js
+++ app/routes/evento-detalle.js
@@ -5,15 +5,18 @@ var EventoDetalleRoute = Ember.Route.extend(AuthenticatedRoute, {
 
   model: function (params) {
     return this.store.findRecord('evento', params.id);
-  },
-
-  setupController: function(controller, model) {
-    var confirmadosPath = window.PimPamPum.hostName + '/api/eventos/' + model.get('id') + '/confirmados';
-    controller.set('confirmados', []);
-    Ember.$.get(confirmadosPath, function (response) {
-      controller.set('confirmados', response.confirmados);
-    });
   }
+
+  ,setupController: function(controller, model) {
+    var confirmadosPath = window.PimPamPum.hostName + '/api/eventos/' + model.get('id') + '/confirmados';
+
+    $.getJSON(confirmadosPath,function(data){
+      controller.set('confirmados', data.confirmados);
+      controller.set('evento', data.evento);
+
+    })
+  }
+
 });
 
 export default EventoDetalleRoute;
